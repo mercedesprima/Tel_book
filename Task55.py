18054438bd74000a96a1fdabf607d0e33b2a7dcb
Ekaterina_P <zazie25@yandex.ru>
2023-04-10 19:31:35 +0300

    Task55 added

--- Task55.py
+++ Task55.py
@@ -6,7 +6,8 @@
 # 3. Пользователь может ввести одну из характеристик для поиска определенной
 # записи(Например имя или фамилию человека)
 # 4. Использование функций. Ваша программа не должна быть линейной
-from Tel_book import functions
+
+import functions
 
 while True:
     print('1. вывод, 2. добавление, 3. поиск, 4. изменение, 5. удаление')
@@ -23,27 +24,5 @@ while True:
         functions.remove_data()
     else:
         break
-
-# with open('book.txt', 'w', encoding='utf-8') as f:
-#     f.write('фио | номер телефона')
-#
-# with open('book.txt', 'a', encoding='utf-8') as f:
-#     f.write('\nфио1 | номер телефона1')
-#
-# with open('book.txt', 'r', encoding='utf-8') as f:
-#     print(f.read())
-# def edited(text: str) -> str:
-#     fio = input()
-#     num = input()
-#     if len(fio) == 0:
-#         fio = text.split(' | ')[0]
-#     if len(num) == 0:
-#         num = text.split(' | ')[1]
-#     return f'{fio} | {num}'
-#
-# a = ['Влад | +7915', 'Анна | +7911']
-# b = 'Влад | +7915'
-# a[a.index(b)] = edited(b)
-# print(a)
 
 
